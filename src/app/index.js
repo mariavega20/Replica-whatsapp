bcca8a382871cc8fe09cb5c84fb72f56b9814ebc
María Jose Vega Petro <mariajosevega72@gmail.com>
2024-03-21 18:07:10 -0500

    funcionalidad de login

--- src/app/index.js
+++ src/app/index.js
@@ -1,31 +1,34 @@
+
 import "./styles/style.scss";
-import image from './assets/images/logo.svg';
-
-//Actualizar las imágenes en el atributo src de las etiquetas
-const logoImage = document.getElementById("logo");
-
-logoImage.setAttribute("src", image);
-
-document.addEventListener('DOMContentLoaded', function() {
-    const app = document.getElementById('app');
-
-    function navigateTo(page) {
-        fetch(page)
-            .then(response => response.text())
-            .then(html => {
-                app.innerHTML = html;
-            });
-    }
-
-    function handleNavigation(event) {
-        event.preventDefault();
-        const targetPage = event.target.getAttribute('href');
-        navigateTo(targetPage);
-    }
-
-    document.querySelectorAll('a').forEach(link => {
-        link.addEventListener('click', handleNavigation);
-    });
-
-    navigateTo('index.html');
+import Swal from 'sweetalert2';
+document.getElementById('form').addEventListener('submit', function(event) {
+  event.preventDefault();
+  
+  const numeroDeCelular = document.getElementById('numeroDeCelular').value;
+  const password = document.getElementById('password').value;
+  const axios = require('axios');
+  
+  if (!numeroDeCelular || !password) {
+    Swal.fire('Error', 'Por favor complete todos los campos.', 'error');
+    return;
+  }
+  
+  // Aquí puedes realizar la solicitud a la API para verificar el inicio de sesión
+  // Por ejemplo:
+  axios.get('https://apifakewhatsapp-dev-qsak.1.us-1.fl0.io/usuarios', {
+    numeroDeCelular: numeroDeCelular,
+    password: password
+  })
+  .then(function (response) {
+    // Manejo de la respuesta exitosa
+    Swal.fire('Bienvenido', 'Inicio de sesión exitoso', 'success');
+    // Redirigir al usuario a la página home
+    setTimeout(function() {
+      window.location.href = '/home.html';
+    }, 1500); // Redireccionar después de 1.5 segundos
+  })
+  .catch(function (error) {
+    // Manejo de errores de la solicitud a la API
+    Swal.fire('Error', 'Error al iniciar sesión. Por favor, inténtalo de nuevo.', 'error');
+  });
 });
